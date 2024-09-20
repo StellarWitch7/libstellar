3cf48a1e7c46a014cd17dd0c05195d1c90f840e1
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-09-19 21:25:16 -0400

    Exposed copper

--- src/main/kotlin/stellarwitch7/libstellar/registry/cca/entity/CCAEntityComponentRegistrar.kt
+++ src/main/kotlin/stellarwitch7/libstellar/registry/cca/entity/CCAEntityComponentRegistrar.kt
@@ -13,11 +13,11 @@ import kotlin.reflect.KClass
 abstract class CCAEntityComponentRegistrar : CCAComponentRegistrar<EntityComponentFactoryRegistry>, EntityComponentInitializer {
     override val registered: ArrayList<(EntityComponentFactoryRegistry) -> Unit> = ArrayList()
 
-    fun <C : Component, T : Entity> register(name: String, c: KClass<C>, factory: (Entity) -> C, t: KClass<T>): ComponentKey<C> {
+    fun <C : Component, T : Entity> register(name: String, c: KClass<C>, factory: (T) -> C, t: KClass<T>): ComponentKey<C> {
         return register(makeKey(name, c), factory, t)
     }
 
-    fun <C : Component, T : Entity> register(key: ComponentKey<C>, factory: (Entity) -> C, t: KClass<T>): ComponentKey<C> {
+    fun <C : Component, T : Entity> register(key: ComponentKey<C>, factory: (T) -> C, t: KClass<T>): ComponentKey<C> {
         return register(key, { registry, key -> registry.registerFor(t.java, key, factory) })
     }
 
---
+++ src/main/kotlin/stellarwitch7/libstellar/registry/cca/world/CCAWorldComponentRegistrar.kt
@@ -0,0 +1,26 @@
+package stellarwitch7.libstellar.registry.cca.world
+
+import net.minecraft.registry.RegistryKey
+import net.minecraft.world.World
+import org.ladysnake.cca.api.v3.component.Component
+import org.ladysnake.cca.api.v3.component.ComponentKey
+import org.ladysnake.cca.api.v3.world.WorldComponentFactoryRegistry
+import org.ladysnake.cca.api.v3.world.WorldComponentInitializer
+import stellarwitch7.libstellar.registry.cca.CCAComponentRegistrar
+import kotlin.reflect.KClass
+
+abstract class CCAWorldComponentRegistrar : CCAComponentRegistrar<WorldComponentFactoryRegistry>, WorldComponentInitializer {
+    override val registered: ArrayList<(WorldComponentFactoryRegistry) -> Unit> = ArrayList()
+
+    fun <C : Component> register(name: String, c: KClass<C>, factory: (World) -> C, t: RegistryKey<World>): ComponentKey<C> {
+        return register(makeKey(name, c), factory, t)
+    }
+
+    fun <C : Component> register(key: ComponentKey<C>, factory: (World) -> C, t: RegistryKey<World>): ComponentKey<C> {
+        return register(key, { registry, key -> registry.registerFor(t, key, factory) })
+    }
+
+    override fun registerWorldComponentFactories(registry: WorldComponentFactoryRegistry) {
+        register(registry)
+    }
+}
